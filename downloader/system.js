32037fe6cef2749ec4a9d06b59872b924356b22d
JuegOStrower <maxios123@hotmail.com.ar>
2017-10-18 20:24:53 -0300

    Update system.js

--- downloader/system.js
+++ downloader/system.js
@@ -8,12 +8,11 @@ $(document).ready(function(){
 			$("#downproj").css("color", "red");
 		}
 	}).on("paste", function(){
-		$(this).val = "";
-		}
+		$(this).val("");
 	});
 	$("#downproj").bind("paste", function(){
 		$(this).css("color", "black");
-		$("#downproj").val = $("#downproj").val().replace("://scratch.mit.edu/projects/", "").replace("https", "").replace("http", "");
+		$("#downproj").val($("#downproj").val().replace("://scratch.mit.edu/projects/", "").replace("https", "").replace("http", ""))
 		if(isNaN($("#downproj").val)){
 			$("#downproj").css("color", "red");
 		}
