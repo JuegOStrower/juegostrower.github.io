c1cbf377323624aa015f2e180f4d722e607e5e6b
JuegOStrower <maxios123@hotmail.com.ar>
2017-10-18 22:39:03 -0300

    Update system.js

--- downloader/system.js
+++ downloader/system.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 		}
 	});
 	if (!window.location.hash.replace("#", "") == ""){
-		$("#downproj").val(window.location.hash.replace("#", "").replace("://scratch.mit.edu/projects/", "").replace("https", "").replace("http", "")).substring(0,10));
+		$("#downproj").val(window.location.hash.replace("#", "").replace("://scratch.mit.edu/projects/", "").replace("https", "").replace("http", "")).substring(0,10);
 		if(isNaN(Number($(this).val()))){
 			$("#downnow").attr("class", "w3-gray w3-center");
 		} else {
