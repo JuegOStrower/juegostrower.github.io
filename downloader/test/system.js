b5cc88ff896481580cd182c722ad7c1e64c72ab0
JuegOStrower <maxios123@hotmail.com.ar>
2017-10-25 20:18:55 -0300

    Update system.js

--- downloader/test/system.js
+++ downloader/test/system.js
@@ -11,126 +11,65 @@ var costumesToDownload = [];
 var totalAssets = 0;
 var completeAssets = 0;
 
-function startDownload(projectId){
-	logMessage("Downloading project: "+projectId);
-	soundId = 0;
-	costumeId = 0;
+function Download(id){
+	logMessage("Downloading project: "+id);
 	totalAssets = 0;
 	completeAssets = 0;
-	soundsToDownload = [];
-  	costumesToDownload = [];
-	id = projectId;
+	assetsToDownload = [];
 	resetProgress();
 	jszip = new JSZip();
-	jszip.comment = "Created with JuegOStrower's Project Downloader";
-	$.get("https://cdn.projects.scratch.mit.edu/internalapi/project/"+projectId+"/get/", function(data){
+	jszip.comment = "Downloaded with JuegOStrower's Project Downloader";
+	$.get("https://cdn.projects.scratch.mit.edu/internalapi/project/"+id+"/get/", function(data){
 		setProgress(10);
 		logMessage("Loaded JSON");
 		project = JSON.parse(data);
-		processSoundsAndCostumes(project);
+		findAssets(project);
 		if(project.hasOwnProperty("children")){
 			for(child in project.children){
-				processSoundsAndCostumes(project.children[child]);
+				findAssets(project.children[child]);
 			}
 		}
+		totalAssets = assetsToDownload.length;
 		logMessage("Found "+totalAssets+" assets");
-		logMessage(costumesToDownload);
-		logMessage(soundsToDownload);
-		logMessage(project);
-	}).fail(perror);
+		jszip.file("project.json", JSON.stringify(project));
+		while (assetsToDownload.length > 0){
+			downloadAsset(assetsToDownload.pop());
+		}
+		exportSb2();
+	}).fail(function(){
+		logMessage("Download error");
+		setProgress(100);
+		reset();
+	});
 }
 
-function downloadCostume(){
-	if(costumesToDownload.length > 0){
-		var current = costumesToDownload.pop();
-		logMessage("Loading asset "+current.costumeName+" ("+completeAssets+"/"+totalAssets+")");
-		JSZipUtils.getBinaryContent(
-			"https://cdn.assets.scratch.mit.edu/internalapi/asset/"+current.baseLayerMD5+"/get/", 
-			function(err, data){
-				if(err) {error();return;}
-				var ext = current.baseLayerMD5.match(/\.[a-zA-Z0-9]+/)[0];
-				jszip.file(current.baseLayerID+ext, data, {binary: true});
-				completeAssets++;
-				setProgress(10+89*(completeAssets/totalAssets));
-				downloadCostume();
-		});
-	} else {
-		downloadSound();
-	}
-}
-
-function downloadSound(){
-	if(soundsToDownload.length > 0){
-		var current = soundsToDownload.pop();
-		logMessage("Loading asset "+current.soundName+" ("+completeAssets+"/"+totalAssets+")");
-		JSZipUtils.getBinaryContent(
-			"https://cdn.assets.scratch.mit.edu/internalapi/asset/"+current.md5+"/get/", 
-			function(err, data){
-				if(err) {error();return;}
-				var ext = current.md5.match(/\.[a-zA-Z0-9]+/)[0];
-				jszip.file(current.soundID+ext, data, {binary: true});
-				completeAssets++;
-				setProgress(10+89*(completeAssets/totalAssets));
-				downloadSound();
-		});
-	} else {
-		logMessage("Loading project title...");
-		$.get("https://scratch.mit.edu/api/v1/project/"+id+"/?format=json", function(data){
-			logMessage("Generating ZIP...");
-			var content = jszip.generate({type:"blob"});
-			logMessage("Saving...");
-			try {
-				saveAs(content, data.title+".sb2");
-			} catch(e){
-				saveAs(content, "project.sb2");
-			}
-			logMessage("Complete");
-			psuccess();
-		}).fail(function(){
-			logMessage("Failed to load project title");
-			logMessage("Generating ZIP...");
-			var content = jszip.generate({type:"blob"});
-			logMessage("Saving...");
-      saveAs(content, "project.sb2");
-			logMessage("Complete");
-			psuccess();
-		});
-	}
-}
-
-function processSoundsAndCostumes(node){
+function findAssets(node){
 	if(node.hasOwnProperty("costumes")){
 		for(var i=0;i<node.costumes.length;i++){
-			var current = node.costumes[i];
-			current.baseLayerID = costumeId;
-			costumeId++;
-			totalAssets++;
-			costumesToDownload.push(current);
+			node.costumes[i].baseLayerID = i;
+			assetsToDownload.push([node.costumes[i].costumeName,node.costumes[i].baseLayerID,node.costumes[i].baseLayerMD5]);
 		}
 	}
 	if(node.hasOwnProperty("sounds")){
 		for(var i=0;i<node.sounds.length;i++){
-			var current = node.sounds[i];
-			current.soundID = soundId;
-			soundId++;
-			totalAssets++;
-			soundsToDownload.push(current);
+			node.sounds[i].soundID = i;
+			assetsToDownload.push([node.sounds[i].soundName,node.sounds[i].soundID,node.sounds[i].md5]);
 		}
 	}
 }
 
-function perror(){
-	logMessage("Download error");
-	reset();
+function downloadAsset(assetData){
+	logMessage("Loading asset "+assetData[0]+" ("+completeAssets+"/"+totalAssets+")");
+	JSZipUtils.getBinaryContent(
+		"https://cdn.assets.scratch.mit.edu/internalapi/asset/"+assetData[2]+"/get/", 
+		function(err, data){
+			if(err) {return;}
+			jszip.file(assetData[1]+"."+assetData[2].split(".")[assetData[2].split(".").length-1], data, {binary: true});
+			completeAssets++;
+			setProgress(10+89*(completeAssets/totalAssets));
+	});
 }
 
-function psuccess(){
-	reset();
-}
-
-function setProgress(perc) {}
-function resetProgress() {}
-function reset(){}
 function logMessage(msg){
 	console.log(msg);
 }
