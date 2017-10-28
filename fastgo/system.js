85e7f173f51331e5d77c62e2ee2c844aeb771029
JuegOStrower <maxios123@hotmail.com.ar>
2017-10-28 20:03:28 -0300

    Create system.js

---
+++ fastgo/system.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+	$("#makenow").click(function(){
+    if ($(this).text() = "Make It!"){
+        $(this).text("Edit");
+        $("#makelink").val("https://go.juegostrower.tk/" + $("#maketype").val() + $("#makeproj").val());
+        $("#maketype").attr("hidden","");
+        $("#makelinkbar").attr("hidden","");
+        $("#makeproj").attr("hidden","");
+      } else {
+        $(this).text("Make It!");
+        $("#makelink").val("https://scratch.mit.edu/");
+        $("#maketype").removeAttr("hidden");
+        $("#makelinkbar").removeAttr("hidden");
+        $("#makeproj").removeAttr("hidden");
+      }
+  })
+
+ $("#makechange").click(function(){
+ if ($("#makechangetext").attr("class") == "w3-hide"){
+      $("#makechangetext").removeAttr("class");
+    } else {
+     $("#makechangetext").attr("class", "w3-hide");
+  }
+});
+  
+	$("#makeproj").bind("input paste", function(){
+		$(this).val($(this).val().replace(/\D/g,'').substring(0,10));
+		if(isNaN(Number($(this).val()))){
+			$(this).css("color", "red");
+			$("#makenow").attr("class", "w3-gray w3-center");
+		} else {
+			$(this).css("color", "black");
+			$("#makenow").attr("class", "w3-gray w3-hover-indigo w3-center w3-button");
+		}
+	});
+})
