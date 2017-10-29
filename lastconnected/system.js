ebee6ba031b0a93058a39c9146a6211c5e07b221
JuegOStrower <maxios123@hotmail.com.ar>
2017-10-29 12:17:53 -0300

    Update system.js

--- lastconnected/system.js
+++ lastconnected/system.js
@@ -1,14 +1,14 @@
 function connnow() {
 if (document.getElementById("connnow").innerText == "Get it!"){
 var xmlHttp = new XMLHttpRequest();
-xmlHttp.open("GET", 'https://api.scratch.mit.edu/users/' + document.getElementById('connuser').value, false);
+xmlHttp.open("GET", 'https://api.scratch.mit.edu/users/' + document.getElementById('connuser').value);
 xmlHttp.send(null);
 document.getElementById("connnow").innerText = "Edit";
-document.getElementById("connuser").value = JSON.parse(xmlHttp.responseText).history.lastReadMessages;
+document.getElementById("connuser").innerText = JSON.parse(xmlHttp.responseText).history.lastReadMessages;
 document.getElementById("connuser").setAttribute("class","w3-hide");
 } else {
 document.getElementById("connnow").innerText = "Get it!";
+document.getElementById("connuser").innerText = "JuegOStrower";
 document.getElementById("connuser").setAttribute("class","w3-input");
-document.getElementById("connuser").value = "JuegOStrower";
 }
 }
