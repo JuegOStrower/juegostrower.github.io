d264e1999bc89f067c69754fef2c5e47c6686020
JuegOStrower <maxios123@hotmail.com.ar>
2017-10-29 12:11:53 -0300

    Update system.js

--- lastconnected/system.js
+++ lastconnected/system.js
@@ -1,14 +1,14 @@
 function connnow() {
-if (document.getElementById("connow").innerText == "Get it!"){
+if (document.getElementById("connnow").innerText == "Get it!"){
 var xmlHttp = new XMLHttpRequest();
 xmlHttp.open("GET", 'https://api.scratch.mit.edu/users/' + document.getElementById('connuser').value, false);
 xmlHttp.send(null);
 document.getElementById("connuser").value = JSON.parse(xmlHttp.responseText).history.lastReadMessages;
 document.getElementById("connuser").addAttribute("hiddden","");
-document.getElementById("connow").innerText = "Edit";
+document.getElementById("connnow").innerText = "Edit";
 } else {
 document.getElementById("connuser").removeAttribute("hiddden");
-document.getElementById("connow").innerText = "Get it!";
+document.getElementById("connnow").innerText = "Get it!";
 document.getElementById("connuser").value = "JuegOStrower";
 }
 }
