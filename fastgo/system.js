62596397ffe4b2defcf8c69762aef5fe8a817554
JuegOStrower <maxios123@hotmail.com.ar>
2017-10-30 19:32:37 -0300

    Update system.js

--- fastgo/system.js
+++ fastgo/system.js
@@ -1,34 +1,33 @@
 $(document).ready(function(){
 	$("#makenow").click(function(){
-    if ($(this).text() == "Make it!"){
-        $(this).text("Edit");
-        $("#makelink").html("<a href='https://go.juegostrower.tk/" + $("#maketype").val() + $("#makeproj").val() + "'>https://go.juegostrower.tk/" + $("#maketype").val() + $("#makeproj").val() + "</a>");
-        $("#makelink").attr("class","w3-text-black");
-        $("#maketype").attr("hidden","");
-        $("#makelinkbar").attr("hidden","");
-        $("#makeproj").attr("class","w3-hide");
-      } else {
-        $(this).text("Make it!");
-        $("#makelink").text("https://scratch.mit.edu/");
-        $("#makelink").removeAttr("class");
-        $("#maketype").removeAttr("hidden");
-        $("#makelinkbar").removeAttr("hidden");
-        $("#makeproj").attr("class","w3-input");
-      }
-  })
+	if ($(this).text() == "Make it!"){
+        	$(this).text("Edit");
+        	$("#makelink").html("<a href='https://go.juegostrower.tk/" + $("#maketype").val() + $("#makeproj").val() + "'>https://go.juegostrower.tk/" + $("#maketype").val() + $("#makeproj").val() + "</a>");
+        	$("#makelink").attr("class","w3-text-black");
+        	$("#maketype").attr("hidden","");
+        	$("#makelinkbar").attr("hidden","");
+        	$("#makeproj").attr("class","w3-hide");
+	} else {
+        	$(this).text("Make it!");
+        	$("#makelink").text("https://scratch.mit.edu/");
+        	$("#makelink").removeAttr("class");
+        	$("#maketype").removeAttr("hidden");
+        	$("#makelinkbar").removeAttr("hidden");
+        	$("#makeproj").attr("class","w3-input");
+	}
+	})
 
- $("#makechange").click(function(){
-	document.getElementById('makechangetext').style.display='block';
-});
-  
+	$("#makechange").click(function(){
+		document.getElementById('makechangetext').style.display='block';
+	});
+
 	$("#makeproj").bind("input paste", function(){
 		$(this).val($(this).val().replace(/\D/g,'').substring(0,10));
-		if(isNaN(Number($(this).val()))){
-			$(this).css("color", "red");
-			$("#makenow").attr("class", "w3-gray w3-center");
-		} else {
-			$(this).css("color", "black");
-			$("#makenow").attr("class", "w3-gray w3-hover-indigo w3-center w3-button");
-		}
+		$("#makenow").attr("class", "w3-gray w3-hover-indigo w3-center w3-button");
 	});
+
+	if (!window.location.hash.replace("#", "") == ""){
+       		$("#makeproj").val($("#makeproj").val().replace(/\D/g,'').substring(0,10));
+		document.getElementById("makenow").click();
+	}
 })
