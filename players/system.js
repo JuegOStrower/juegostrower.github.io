0f6b16b0f47ca0349df7701ae474d0530d79504f
JuegOStrower <maxios123@hotmail.com.ar>
2017-11-29 21:02:52 -0300

    Update system.js

--- players/system.js
+++ players/system.js
@@ -27,4 +27,7 @@ $(document).ready(function(){
   if (!window.location.hash.replace("#", "") == ""){
   	$("#makeproj").val($("#makeproj").val().replace(/\D/g,'').substring(0,10));
   }
+	$("#makeproj").bind("input paste", function(){
+		$(this).val($(this).val().replace(/\D/g,'').substring(0,10));
+	});
 })
