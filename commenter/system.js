3d00dca6b13e731d159a11f0171c8f67293674dc
JuegOStrower <maxios123@hotmail.com.ar>
2017-12-06 20:25:51 -0300

    Bugs fixed

--- commenter/system.js
+++ commenter/system.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
 			followList = [];
 			document.getElementById("percBar").style.width = '0%';
 			document.getElementById("commnow").innerHTML = "Loading...";
-			$.get("https://scratch.mit.edu/session/",function (data){try {token = data.user.token;} catch (e){document.getElementById("commnow").innerHTML = "Please log in to Scratch first"; throw "Please log in to Scratch first";ready();}});
+			$.get("https://scratch.mit.edu/session/",function (data){try {token = JSON.parse(data).user.token;} catch (e){document.getElementById("commnow").innerHTML = "Please log in to Scratch first"; throw "Please log in to Scratch first";ready();}});
 			$.get("https://scratch.mit.edu/users/" + user + "/followers/?page=" + page, loaded).fail(function () {document.getElementById("commnow").innerHTML = "That user doesn't exists"; throw "That user doesn't exists";ready();});
 		}
 	});
