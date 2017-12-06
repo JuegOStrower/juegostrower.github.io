f74131b968393d143172afb88be059f5ed3f4210
JuegOStrower <maxios123@hotmail.com.ar>
2017-12-06 19:40:51 -0300

    Bugs fixed

--- commenter/system.js
+++ commenter/system.js
@@ -65,8 +65,8 @@ function continueCode() {
 			throw "There was an error posting the comments.";
 		}
 	};
-	xhttp.setRequestHeader("X-CSRFToken", "IEi6SVeAu8MCHAqpgT5d7Q8HtRJwFYiU")
 	xhttp.open("POST", "https://scratch.mit.edu/site-api/comments/user/" + followList[i] + "/add/", true);
+	xhttp.setRequestHeader("X-CSRFToken", "IEi6SVeAu8MCHAqpgT5d7Q8HtRJwFYiU")
 	xhttp.send(JSON.stringify({"content":message.replace(/-USER-/g, followList[i]),"parent_id":"","commentee_id":""}));
 	console.log("Posting comment to " + followList[i] + ", user " + (i + 1) + "/" + count);
 }
